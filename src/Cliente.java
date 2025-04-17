451052c0911619e8e94fe72ffbc40aa399816773
marcaiz <marcarinisimone05@gmail.com>
2025-04-17 15:35:29 +0200

    Update Cliente.java

--- src/Cliente.java
+++ src/Cliente.java
@@ -3,123 +3,114 @@ package src;
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * Classe che rappresenta un Cliente, estendendo la classe Utente.
+ * Implementa l'interfaccia CSVWritable per la serializzazione in formato CSV.
+ * Contiene informazioni sui ristoranti preferiti e sulle recensioni effettuate.
+ */
 public class Cliente extends Utente implements CSVWritable {
-    // Campi privati
     private List<Ristorante> preferiti;
     private List<Recensione> recensioni;
 
+    /**
+     * Costruttore della classe Cliente con elenco di preferiti e recensioni.
+     *
+     * @param nome Nome del cliente.
+     * @param cognome Cognome del cliente.
+     * @param username Username del cliente.
+     * @param passwordCifrata Password cifrata del cliente.
+     * @param dataNascita Data di nascita del cliente.
+     * @param domicilio Domicilio del cliente.
+     * @param ruolo Ruolo del cliente (false, perché è un cliente).
+     * @param preferiti Lista dei ristoranti preferiti.
+     * @param recensioni Lista delle recensioni fatte dal cliente.
+     */
     public Cliente(String nome, String cognome, String username, String passwordCifrata, LocalDate dataNascita,
-            String domicilio, boolean ruolo, List<Ristorante> preferiti, List<Recensione> recensioni) {
+                   String domicilio, boolean ruolo, List<Ristorante> preferiti, List<Recensione> recensioni) {
         super(nome, cognome, username, passwordCifrata, dataNascita, domicilio, ruolo);
         this.preferiti = preferiti;
         this.recensioni = recensioni;
     }
 
-    // Costruttore
+    /**
+     * Costruttore della classe Cliente senza elenco di preferiti e recensioni.
+     *
+     * @param nome Nome del cliente.
+     * @param cognome Cognome del cliente.
+     * @param username Username del cliente.
+     * @param passwordCifrata Password cifrata del cliente.
+     * @param dataNascita Data di nascita del cliente.
+     * @param domicilio Domicilio del cliente.
+     */
     public Cliente(String nome, String cognome, String username, String passwordCifrata, LocalDate dataNascita,
                    String domicilio) {
         super(nome, cognome, username, passwordCifrata, dataNascita, domicilio, false);
     }
 
-    // Getter e Setter per preferiti
+    /**
+     * Restituisce la lista dei ristoranti preferiti del cliente.
+     *
+     * @return Lista dei ristoranti preferiti.
+     */
     public List<Ristorante> getPreferiti() {
         return preferiti;
     }
 
+    /**
+     * Imposta la lista dei ristoranti preferiti del cliente.
+     *
+     * @param preferiti Lista dei ristoranti preferiti.
+     */
     public void setPreferiti(List<Ristorante> preferiti) {
         this.preferiti = preferiti;
     }
 
-    // Getter e Setter per recensioni
+    /**
+     * Restituisce la lista delle recensioni fatte dal cliente.
+     *
+     * @return Lista delle recensioni.
+     */
     public List<Recensione> getRecensioni() {
         return recensioni;
     }
 
+    /**
+     * Imposta la lista delle recensioni fatte dal cliente.
+     *
+     * @param recensioni Lista delle recensioni.
+     */
     public void setRecensioni(List<Recensione> recensioni) {
         this.recensioni = recensioni;
     }
 
-    // Getter e Setter per nome, cognome, username, passwordCifrata, dataNascita, domicilio (ereditati dalla classe Utente)
-    
-    // Getter e Setter per nome
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    // Getter e Setter per cognome
-    public String getCognome() {
-        return cognome;
-    }
-
-    public void setCognome(String cognome) {
-        this.cognome = cognome;
-    }
-
-    // Getter e Setter per username
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    // Getter e Setter per passwordCifrata
-    public String getPasswordCifrata() {
-        return passwordCifrata;
-    }
-
-    public void setPasswordCifrata(String passwordCifrata) {
-        this.passwordCifrata = passwordCifrata;
-    }
-
-    // Getter e Setter per dataNascita
-    public LocalDate getDataNascita() {
-        return dataNascita;
-    }
-
-    public void setDataNascita(LocalDate dataNascita) {
-        this.dataNascita = dataNascita;
-    }
-
-    // Getter e Setter per domicilio
-    public String getDomicilio() {
-        return domicilio;
-    }
-
-    public void setDomicilio(String domicilio) {
-        this.domicilio = domicilio;
-    }
-
-    // Metodo toString
+    /**
+     * Restituisce una rappresentazione testuale del cliente.
+     *
+     * @return Stringa contenente le informazioni del cliente.
+     */
     @Override
     public String toString() {
         return "Cliente {" +
-            "nome='" + (nome != null ? nome : "") + '\'' +
-            ", cognome='" + (cognome != null ? cognome : "") + '\'' +
-            ", username='" + (username != null ? username : "") + '\'' +
-            ", data di nascita=" + (dataNascita != null ? dataNascita.toString() : "non disponibile") +
-            ", domicilio='" + (domicilio != null ? domicilio : "") + '\'' +
-            '}';
+                "nome='" + (nome != null ? nome : "") + '\'' +
+                ", cognome='" + (cognome != null ? cognome : "") + '\'' +
+                ", username='" + (username != null ? username : "") + '\'' +
+                ", data di nascita=" + (dataNascita != null ? dataNascita.toString() : "non disponibile") +
+                ", domicilio='" + (domicilio != null ? domicilio : "") + '\'' +
+                '}';
     }
 
-    // Metodo getRuolo
-    
-
-    // Metodo toCSV per serializzare l'oggetto Cliente in formato CSV
+    /**
+     * Restituisce la rappresentazione del cliente in formato CSV.
+     *
+     * @return Stringa contenente i dati del cliente separati da virgole.
+     */
     @Override
     public String toCSV() {
         return (nome != null ? nome : "") + "," +
-            (cognome != null ? cognome : "") + "," +
-            (username != null ? username : "") + "," +
-            (passwordCifrata != null ? passwordCifrata : "") + "," +
-            (dataNascita != null ? dataNascita.toString() : "1900-01-01") + "," +
-            (domicilio != null ? domicilio : ""+ "," + ruolo);
+               (cognome != null ? cognome : "") + "," +
+               (username != null ? username : "") + "," +
+               (passwordCifrata != null ? passwordCifrata : "") + "," +
+               (dataNascita != null ? dataNascita.toString() : "1900-01-01") + "," +
+               (domicilio != null ? domicilio : "") + "," + ruolo;
     }
 }
-
-
